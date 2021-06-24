4b4c07efd232cc46d47452086b98d821f276aea2
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-24 11:06:14 -0400

    fixing the routes

--- routes/API/productRoutes.js
+++ routes/API/productRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const itemsController = require('../../controllers/itemsController');
-const cartController = require('../../controllers/cartController');
 
 // Routing to the homepage and a get to get all items in the database using the controllers
 
@@ -10,14 +9,5 @@ router.route('/').get(itemsController.findAll);
 
 router.route('/:ItemId').get(itemsController.findOne);
 
-router.route('/').post(cartController.create);
-
-//router.route('/userCart/:id').get(cartController.findOne);
-
-router.route('/:id').delete(cartController.deleteOne);
-
-router.route('/').put(cartController.updateOne);
-
-router.route('/completeOrder').post(cartController.createOrder);
 
 module.exports = router;
