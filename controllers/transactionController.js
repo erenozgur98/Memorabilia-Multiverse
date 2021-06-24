b9c284a4b0914c5dbd83c4973c03df080a5dd75d
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-24 11:01:48 -0400

    fixed a bug

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -11,7 +11,7 @@ module.exports = {
             });
     },
     create: function (req, res) {
-        Transaction.create(,
+        Transaction.create(
             req.body
         )
             .then((newTransaction) => {
