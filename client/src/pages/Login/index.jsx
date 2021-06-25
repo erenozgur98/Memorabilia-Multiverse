45726965863d2a8daedf4f898347fc2a015d3fc2
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-25 18:49:18 -0400

    adding signup and login pages to test them out

---
+++ client/src/pages/Login/index.jsx
@@ -0,0 +1,30 @@
+import React, { useRef } from 'react'
+import API from '../../utils/API';
+import { Container } from 'react-bootstrap';
+
+function Login() {
+    const username = useRef();
+    const password = useRef();
+
+    const loginHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const newLogin = await API.login({ email: email.current.value, username: username.current.value, password: password.current.value });
+            console.log(newLogin);
+        } catch (err) {
+            console.log('Login error: ', err);
+        }
+    };
+
+    return (
+        <Container>
+            <form action="/login" method="POST">
+                <input type="text" name="username" placeholder="username" />
+                <input type="password" name="password" placeholder="password" />
+                <button onClick={loginHandler}>Login</button>
+            </form>
+        </Container>
+    )
+}
+
+export default Login;
---
+++ client/src/pages/Signup/index.jsx
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react'
+import API from '../../utils/API';
+import { Container } from 'react-bootstrap';
+
+function SignUp() {
+    const email = useRef();
+    const username = useRef();
+    const password = useRef();
+
+    const signupHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const newUser = await API.signup({ email: email.current.value, username: username.current.value, password: password.current.value });
+            console.log(newUser);
+        } catch (err) {
+            console.log('SignUp error: ', err);
+        }
+    };
+
+    return (
+        <Container>
+            <form action="/login" method="POST">
+                <input type="text" name='email' placeholder='email' />
+                <input type="text" name="username" placeholder="username" />
+                <input type="password" name="password" placeholder="password" />
+                <button onClick={signupHandler}>Login</button>
+            </form>
+        </Container>
+    )
+}
+
+export default SignUp;
