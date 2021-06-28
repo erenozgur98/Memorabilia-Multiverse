456333fa79b719bc4f429b77bba8b505066b82fa
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-28 11:21:17 -0400

    fixed

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Navbar, Nav, NavbarBrand } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './style.css';
 
 function Header() {
     return (
