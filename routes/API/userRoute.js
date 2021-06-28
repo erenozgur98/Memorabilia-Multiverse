44ddc1486d9acd4ab09f648d0e64dbaf4dd5a28e
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-28 10:14:39 -0400

    login/signup working

--- routes/API/userRoute.js
+++ routes/API/userRoute.js
@@ -26,74 +26,20 @@ router.get('/login', async (req, res) => {
 
 router.post('/signup', async (req, res) => {
     try {
-        const { email, username, password } = req.body;
+        const user = await User.findOne({ where: { username: req.body.username }});
+        if (user) {
+            res.status(400).json({ message: 'That username is taken buddy '});
+        } else {
+            const newUser = await User.create(req.body);
+            delete newUser.password;
+            res.json(newUser);
 
-        const userData = {
-            email: email.toLowerCase(),
-            username,
-            password
         };
-
-        
-        // const existingUsername = await User.findOne({ username: req.body.username });
-
-        // if (existingUsername) {
-        //     return res.status(400).json({ message: 'Username already exists' });
-        // };
-
-        const newUser = await User.create(req.body);
-        delete newUser.password;
-        const savedUser = newUser.save();
-        res.json(newUser);
-        console.log('New User:', newUser, 'Saved User: ', savedUser);
-       
     } catch (err) {
         console.log(err);
         res.status(404).json(err);
     }
 })
-
-// router.post('/signup', async (req, res) => {
-//     try {
-//         const { email, username } = req.body;
-//         const hashedPassword = await hashPassword(req.body.password);
-
-//         const userData = { email: email.toLowerCase(), username, password: hashedPassword };
-
-//         const existingUsername = await User.findOne({ username: req.body.username });
-
-//         if (existingUsername) {
-//             return res.status(400).json({ message: 'Username already exists' });
-//         };
-
-//         const newUser = new User(userData);
-//         const savedUser = newUser.save();
-
-//         if (savedUser) {
-//             const token = createToken(savedUser);
-
-//             const { username, email } = savedUser;
-
-//             const userInfo = { username, email };
-//             console.log('TESTING IF USERINFO GOT THE INFO: ', userInfo.username);
-
-//             return res.json({
-//                 message: 'User created',
-//                 token,
-//                 userInfo
-//             })
-//         } else {
-//             return res.status(400).json({
-//                 message: 'There was a problem'
-//             });
-//         };
-//     } catch (err) {
-//       return res.status(404).json({
-//         message: 'There was a problem creating your account'
-//       });
-//     }
-// });
-
 
 router.post('/login', async (req, res) => {
     try {
@@ -101,12 +47,6 @@ router.post('/login', async (req, res) => {
 
         if (!user) {
             res.status(400).json({ msg: 'Incorrect Username or Password ' });
-            return;
-        };
-
-        const validatePassword = await user.checkPassword(req.body.password);
-        if (!validatePassword) {
-            res.status(400).json({ msg: 'Incorrect Username of Password ' });
             return;
         };
 
